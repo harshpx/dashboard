6490ef15c857b7762855a7e47d943ccc8c07cc77
harshpx <harsh.rzf@gmail.com>
2024-04-19 06:05:29 +0530

    changed login wallpaper

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     }
   },[userName,savedUser])
   return (
-    <div className='min-h-screen min-w-full flex items-center justify-center bg-neutral-800 bg-[url("/home/harsh/Documents/dashboard/src/assets/bg/wall3.jpg")] bg-cover'>
+    <div className='min-h-screen min-w-full flex items-center justify-center bg-neutral-800 bg-[url("https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover'>
       <div className='rounded-lg bg-neutral-800 p-6 h-52 w-2/3 md:w-1/2 xl:w-1/4 flex flex-col gap-5 items-center justify-evenly'>
         <div className='flex gap-1 items-center justify-between text-white text-xl'>
           <LuSettings2 size={24}/>
